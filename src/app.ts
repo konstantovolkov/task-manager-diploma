2d8253936f9f2b5d76d084507774389e01a0130e
Konstantin Volkov <konstantin_volkov@epam.com>
2020-03-17 16:51:16 +0300

    Add use Task, UserTask, Subject entities

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import { Server } from './utils/Server';
 import { UserController } from './domains/User/User.controller';
 import { createConnection } from 'typeorm';
 import { User } from './domains/User/User.model';
+import 'reflect-metadata';
+import { UserTask } from './domains/UserTask/UserTask.model';
+import { Subject } from './domains/Subject/Subject.model';
+import { Task } from './domains/Task/Task.model';
 
 (async () => {
   const connection = await createConnection({
@@ -12,7 +16,7 @@ import { User } from './domains/User/User.model';
     password: '1234',
     database: 'konstantin_volkov',
     synchronize: true,
-    entities: [User]
+    entities: [User, Task, UserTask, Subject]
   });
 
   console.log(connection.isConnected);
